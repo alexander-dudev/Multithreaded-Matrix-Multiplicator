f4b0a321ad52d0c32e3c9e8daf08c8f9a20470f6
Alexander Dudev <alexander.dudev@gmail.com>
2019-06-30 17:23:09 +0300

    add tests

---
+++ tests/InputParametersParserTest.java
@@ -0,0 +1,153 @@
+import org.junit.Test;
+import java.io.IOException;
+import static org.junit.Assert.*;
+
+public class InputParametersParserTest {
+
+    @Test(expected = AssertionError.class)
+    public void noneObligatoryParametersAreProvided() throws IOException {
+        String arguments[] = new String[5];
+        arguments[0] = "-t";
+        arguments[1] = "-5";
+        arguments[2] = "-g";
+        arguments[3] = "-3";
+        arguments[4] = "-q";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void notAllobligatoryParametersAreProvided() throws IOException {
+        String arguments[] = new String[5];
+        arguments[0] = "-m";
+        arguments[1] = "-512";
+        arguments[2] = "-n";
+        arguments[3] = "-1024";
+        arguments[4] = "-q";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void unsupportedOptionIsProvided() throws IOException {
+        String arguments[] = new String[7];
+        arguments[0] = "-i";
+        arguments[1] = "test-identityMatrix.txt";
+        arguments[2] = "-g";
+        arguments[3] = "2";
+        arguments[4] = "-q";
+        arguments[5] = "-p";
+        arguments[6] = "3";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void emptyInputFileIsProvided() throws IOException {
+        String arguments[] = new String[7];
+        arguments[0] = "-i";
+        arguments[1] = "test-emptyFile.txt";
+        arguments[2] = "-g";
+        arguments[3] = "2";
+        arguments[4] = "-q";
+        arguments[5] = "-t";
+        arguments[6] = "3";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test
+    public void allParametersAreProvidedValidlyAndParsedCorrectly() throws IOException {
+        String arguments[] = new String[15];
+        arguments[0] = "-m";
+        arguments[1] = "512";
+        arguments[2] = "-n";
+        arguments[3] = "1024";
+        arguments[4] = "-k";
+        arguments[5] = "2048";
+        arguments[6] = "-i";
+        arguments[7] = "test-identityMatrix.txt";
+        arguments[8] = "-o";
+        arguments[9] = "output.txt";
+        arguments[10] = "-q";
+        arguments[11] = "-g";
+        arguments[12] = "3";
+        arguments[13] = "-t";
+        arguments[14] = "5";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+        assertEquals("The number of rows in the left matrix must be 512", parser.getNumberOfRowsInLeftMatrix(), 512);
+        assertEquals("The number of columns in the left matrix must be 1024", parser.getNumberOfColumnsInLeftMatrix(), 1024);
+        assertEquals("The number of columns in the right matrix must be 2048", parser.getNumberOfColumnsInRightMatrix(), 2048);
+        assertTrue(parser.matrixDimensionsAreProvided());
+
+        assertEquals("The input filename must be: test-identityMatrix.txt", parser.getInputFilename(), "test-identityMatrix.txt");
+        assertEquals("The output filename must be: output.txt", parser.getOutputFilename(), "output.txt");
+        assertTrue(parser.inputFilenameIsProvided());
+        assertTrue(parser.outputFilenameIsProvided());
+
+        assertTrue(parser.isQuietOptionSet());
+        assertEquals("Granularity level must be equal to 3", parser.getGranularityLevel(), 3);
+
+        assertEquals("The maximum number of running threads must be equal to 5", parser.getMaximumNumberOfRunningThreads(), 5);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void negativeDimensionIsProvided() throws IOException {
+        String arguments[] = new String[7];
+        arguments[0] = "-m";
+        arguments[1] = "1024";
+        arguments[2] = "-n";
+        arguments[3] = "512";
+        arguments[4] = "-k";
+        arguments[5] = "-2048";
+        arguments[6] = "q";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void negativeNumberOfThreadsIsProvided() throws IOException {
+        String arguments[] = new String[8];
+        arguments[0] = "-m";
+        arguments[1] = "1024";
+        arguments[2] = "-n";
+        arguments[3] = "512";
+        arguments[4] = "-k";
+        arguments[5] = "2048";
+        arguments[6] = "-t";
+        arguments[7] = "-3";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void negativeGranularityLevelIsProvided() throws IOException {
+        String arguments[] = new String[8];
+        arguments[0] = "-m";
+        arguments[1] = "1024";
+        arguments[2] = "-n";
+        arguments[3] = "512";
+        arguments[4] = "-k";
+        arguments[5] = "2048";
+        arguments[6] = "-g";
+        arguments[7] = "-5";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void invalidInputMatrixIsProvided() throws IOException {
+        String arguments[] = new String[7];
+        arguments[0] = "-i";
+        arguments[1] = "test-invalidMatrix.txt";
+        arguments[2] = "-q";
+        arguments[3] = "-t";
+        arguments[4] = "5";
+        arguments[5] = "-g";
+        arguments[6] = "5";
+
+        InputParametersParser parser = new InputParametersParser(arguments);
+    }
+
+}
---
+++ tests/MainTest.java
@@ -0,0 +1,158 @@
+import org.junit.Test;
+import java.io.*;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void multiplyMatricesFromFileUsingOneThreadAndGranularityOne() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-i";
+        arguments[1] = "test-randomMatrices.txt";
+        arguments[2] = "-t";
+        arguments[3] = "1";
+        arguments[4] = "-g";
+        arguments[5] = "1";
+        arguments[6] = "-o";
+        arguments[7] = "output.txt";
+        Main.main(arguments);
+
+        testIfResultInFileIsCorrect("output.txt", getExpectedMatrixAccordingToFileWithRandomMatrices());
+    }
+
+    @Test
+    public void multiplyMatricesFromFileUsingThreeThreadsAndGranularityOne() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-i";
+        arguments[1] = "test-randomMatrices.txt";
+        arguments[2] = "-t";
+        arguments[3] = "3";
+        arguments[4] = "-g";
+        arguments[5] = "1";
+        arguments[6] = "-o";
+        arguments[7] = "output.txt";
+        Main.main(arguments);
+
+        testIfResultInFileIsCorrect("output.txt", getExpectedMatrixAccordingToFileWithRandomMatrices());
+    }
+
+    @Test
+    public void multiplyMatricesFromFileUsingOneThreadAndGranularityThree() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-i";
+        arguments[1] = "test-randomMatrices.txt";
+        arguments[2] = "-t";
+        arguments[3] = "1";
+        arguments[4] = "-g";
+        arguments[5] = "3";
+        arguments[6] = "-o";
+        arguments[7] = "output.txt";
+        Main.main(arguments);
+
+        testIfResultInFileIsCorrect("output.txt", getExpectedMatrixAccordingToFileWithRandomMatrices());
+    }
+
+    @Test
+    public void multiplyMatricesFromFileUsingThreeThreadsAndGranularityThree() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-i";
+        arguments[1] = "test-randomMatrices.txt";
+        arguments[2] = "-t";
+        arguments[3] = "3";
+        arguments[4] = "-g";
+        arguments[5] = "3";
+        arguments[6] = "-o";
+        arguments[7] = "output.txt";
+        Main.main(arguments);
+
+        testIfResultInFileIsCorrect("output.txt", getExpectedMatrixAccordingToFileWithRandomMatrices());
+    }
+
+    @Test
+    public void multiplyIdentityMatrixWithRandomFromFile() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-i";
+        arguments[1] = "test-identityMatrix.txt";
+        arguments[2] = "-t";
+        arguments[3] = "3";
+        arguments[4] = "-g";
+        arguments[5] = "2";
+        arguments[6] = "-o";
+        arguments[7] = "output.txt";
+        Main.main(arguments);
+
+        testIfResultInFileIsCorrect("output.txt", getExpectedMatrixAccordingToFileWithIdentityMatrix());
+    }
+
+    // it is tested and confirmed that the program correctly multiplies matices that are given in a file;
+    // using that, the multiplication of randomly generated matrices can be tested
+    @Test
+    public void multiplyRandomlyGeneratedMatrices() throws IOException, InterruptedException {
+        String arguments[] = new String[8];
+        arguments[0] = "-m";
+        arguments[1] = "3";
+        arguments[2] = "-n";
+        arguments[3] = "3";
+        arguments[4] = "-k";
+        arguments[5] = "3";
+        arguments[6] = "-o";
+        arguments[7] = "outputFromRandomlyGeneratedMatrices.txt";
+        Main.main(arguments);
+
+        String otherArguments[] = new String[4];
+        otherArguments[0] = "-i";
+        otherArguments[1] = "randomlyGeneratedMatrices.txt";
+        otherArguments[2] = "-o";
+        otherArguments[3] = "correctOutput.txt";
+        Main.main(otherArguments);
+
+        MatrixReader reader = new MatrixReader();
+        MatrixOfDoubles expectedMatrix = reader.readSingleMatrixFrom("correctOutput.txt");
+        testIfResultInFileIsCorrect("outputFromRandomlyGeneratedMatrices.txt", expectedMatrix);
+    }
+
+    private void testIfResultInFileIsCorrect(String outputFilename, MatrixOfDoubles expectedMatrix) throws IOException, InterruptedException {
+        MatrixReader reader = new MatrixReader();
+        MatrixOfDoubles resultingMatrix = reader.readSingleMatrixFrom(outputFilename);
+        testWhetherMatricesAreEqual(resultingMatrix, expectedMatrix);
+    }
+
+    private MatrixOfDoubles getExpectedMatrixAccordingToFileWithRandomMatrices() {
+        MatrixOfDoubles expectedMatrix = new MatrixOfDoubles(2, 3);
+        expectedMatrix.setElementAt(0,0 , 3);
+        expectedMatrix.setElementAt(0,1, 1);
+        expectedMatrix.setElementAt(0,2,2);
+        expectedMatrix.setElementAt(1,0, 6);
+        expectedMatrix.setElementAt(1,1, 2);
+        expectedMatrix.setElementAt(1,2, 4);
+        return expectedMatrix;
+    }
+
+    private MatrixOfDoubles getExpectedMatrixAccordingToFileWithIdentityMatrix() {
+        MatrixOfDoubles expectedMatrix = new MatrixOfDoubles(3, 3);
+        expectedMatrix.setElementAt(0,0 , 1);
+        expectedMatrix.setElementAt(0,1, 1);
+        expectedMatrix.setElementAt(0,2,4);
+        expectedMatrix.setElementAt(1,0, 1);
+        expectedMatrix.setElementAt(1,1, 1);
+        expectedMatrix.setElementAt(1,2, 7);
+        expectedMatrix.setElementAt(2,0, 1);
+        expectedMatrix.setElementAt(2,1, 1);
+        expectedMatrix.setElementAt(2,2, 1);
+        return expectedMatrix;
+    }
+
+    private void testWhetherMatricesAreEqual(MatrixOfDoubles resultingMatrix, MatrixOfDoubles expectedMatrix) {
+        assertEquals("The number of rows in both matrices must be the same!",
+                resultingMatrix.getNumberOfRows(), expectedMatrix.getNumberOfRows());
+        assertEquals("The number of columns in both matrices must be the same!",
+                resultingMatrix.getNumberOfColumns(), expectedMatrix.getNumberOfColumns());
+
+        for (int i = 0; i < resultingMatrix.getNumberOfRows(); i++) {
+            for (int j = 0; j < resultingMatrix.getNumberOfColumns(); j++) {
+                assertEquals("The respective elements on position [" + i + ", " + j + "] must be the same!",
+                        resultingMatrix.getElementAt(i,j), expectedMatrix.getElementAt(i,j), 0.01);
+            }
+        }
+    }
+}
